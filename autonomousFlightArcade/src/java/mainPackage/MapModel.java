c76e854e59b6e5ade610c6c3c828f0a6efc68688
jorgefhn <78788085+jorgefhn@users.noreply.github.com>
2023-05-14 16:49:45 +0200

    Update MapModel.java

--- autonomousFlightArcade/src/java/mainPackage/MapModel.java
+++ autonomousFlightArcade/src/java/mainPackage/MapModel.java
@@ -1,60 +1,11 @@
  
-import jason.environment.grid.GridWorldModel;
-import jason.environment.grid.Location;
 import java.util.Random;
 
 /** class that implements the Model of Domestic Robot application */
-public class MapModel extends GridWorldModel {
+public class MapModel {
 
      
-    // constants for the grid objects
-    public static final int HEALTH = 16;
-
-    // the grid size
-    public static final int GSize = 7;
-
-    boolean healthPicked   = false; // whether the health package is picked up
-
-
-
-    Location lLeftUp = new Location(0,0); // left top corner 
-    Location lRightUp = new Location(GSize-1,0); // right top corner
-    Location lLeftDown = new Location(0,GSize-1); // left bottom corner 
-    Location lRightDown = new Location(GSize-1,GSize-1); // right bottom corner 
-    Location lHealth  = new Location(GSize/2,GSize/2);
-
    
-
-    
-
-
-
-   
-    public MapModel() {
-        // create a 7x7 grid with one mobile agent
-        super(GSize, GSize, 2);
-
-        // ag code 0 means the drone1
-        setAgPos(0, 0, 0);
-		// ag code 1 means the drone2
-		Location dLoc2 = new Location(GSize-1,GSize-1);
-        setAgPos(1, dLoc2);
-
-        // adding location to the health package
-        add(HEALTH,lHealth);
-                
-        
-    }
-
-
-    boolean pickHealth(){
-        if (!healthPicked){
-            healthPicked = true;
-            return true;
-        }else{
-            return false;
-        }
-    }
 	
 	Point3D getSafePosition(Point3D A, Point3D B){
 		// método auxiliar para calcular punto simétrico
