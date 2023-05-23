3f7764925979f66c9dc9686c26368a1af6604217
jorgefhn <78788085+jorgefhn@users.noreply.github.com>
2023-05-24 01:14:47 +0200

    Update MapModel.java

--- autonomousFlightArcade/src/java/mainPackage/MapModel.java
+++ autonomousFlightArcade/src/java/mainPackage/MapModel.java
@@ -1,35 +1,62 @@
  
-import java.util.Random;
+import javax.json.JsonArray;
 
 /** class that implements the Model of Domestic Robot application */
 public class MapModel {
 
      
-   
-	
-	Point3D getSafePosition(Point3D A, Point3D B){
-		// método auxiliar para calcular punto simétrico
-		System.out.println("Getting safe position: "+A+" "+B);
-		Point3D AB = new Point3D(A.x-B.x,A.y-B.y,A.z-B.z); // coordenadas del vector AB
-		Point3D AC = new Point3D(A.x+ 2*AB.x,A.y +2*AB.y,A.z+2*AB.z); // coordenadas del vector AC
-		Point3D C = new Point3D(A.x + AC.x, A.y + AC.y, A.z + AC.z); // este es el nuevo punto a devolver 
-		System.out.println("New safe position "+C);
+   Point3D getPackagePos(JsonArray packageList)
+   {
+	   System.out.println(packageList);
+	   System.out.println(packageList.get(0));
 
-		return C;
+	   Point3D dest = new Point3D(0.0,0.0,0.0);
+	   
+	   return dest;
+	   
+   }
+	 
+	Point3D getSafePosition(String agent){
+		Point3D destiny = new Point3D(0.0,0.0,0.0);
+		
+		if (agent.equals("drone1"))
+		{
+			// move towards start point
+			destiny.setX(974.0);
+			destiny.setY(122.0);
+			destiny.setZ(-454.0);
+			
+		}
+		if (agent.equals("drone2"))
+		{
+			// move towards end point
+			destiny.setX(-494.1);
+			destiny.setY(100.0);
+			destiny.setZ(994.7);
+		}
+		return destiny;
 		
 	}
 	
-	Point3D getNewPosition(Point3D position){
-		// método auxiliar para calcular un nuevo punto aleatorio en su zona
-		// genera una nueva posición aleatoria en un rango de 40, con coordenada z positiva 
-		Random rand = new Random();
-        double x = position.getX() + (rand.nextDouble() * 80 - 40 ) + 10; // Rango [-40, 40]
-        double y = position.getX() + (rand.nextDouble() * 80 - 40) + 10; // Rango [-40, 40]
-        double z = position.getX() +(rand.nextDouble() * 80 - 40) + 10; // Rango [-40, 40]
+	Point3D getNewPosition(String agent,Point3D d1pos, Point3D d2pos){
 		
-		Point3D randomPoint = new Point3D(x,y,z); // este es el nuevo punto a devolver 
-		return randomPoint;
-		
+		// Method to calculate the new position. The target of the drone will be the rival's position
+		Point3D destiny = new Point3D(0.0,0.0,0.0);
+
+		if (agent.equals("drone1"))
+		{
+			destiny.setX(d2pos.getX());
+			destiny.setY(d2pos.getY());
+			destiny.setZ(d2pos.getZ());
+			
+		}
+		if (agent.equals("drone2"))
+		{
+			destiny.setX(d1pos.getX());
+			destiny.setY(d1pos.getY());
+			destiny.setZ(d1pos.getZ());
+		}
+		return destiny;
 	}
 
     
