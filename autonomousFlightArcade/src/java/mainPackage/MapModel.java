9ab875d5afa541875cbf620c5e165d2f5a4b2996
jorgefhn <78788085+jorgefhn@users.noreply.github.com>
2023-06-03 17:21:39 +0200

    Update MapModel.java

--- autonomousFlightArcade/src/java/mainPackage/MapModel.java
+++ autonomousFlightArcade/src/java/mainPackage/MapModel.java
@@ -16,49 +16,49 @@ public class MapModel {
 	   
    }
 	 
-	Point3D getSafePosition(String agent){
-		Point3D destiny = new Point3D(0.0,0.0,0.0);
-		
-		if (agent.equals("drone1"))
-		{
-			// move towards start point
-			destiny.setX(974.0);
-			destiny.setY(122.0);
-			destiny.setZ(-454.0);
+	Point3D getSafePositiond1(){
+		Point3D destinyd1 = new Point3D(0.0,0.0,0.0);
+	
+		// d1 to base
+		destinyd1.setX(974.0);
+		destinyd1.setY(122.0);
+		destinyd1.setZ(-454.0);
 			
-		}
-		if (agent.equals("drone2"))
-		{
-			// move towards end point
-			destiny.setX(-494.1);
-			destiny.setY(100.0);
-			destiny.setZ(994.7);
-		}
+	
+		return destinyd1;
+		
+	}
+	
+	Point3D getSafePositiond2(){
+		Point3D destinyd2 = new Point3D(0.0,0.0,0.0);
+
+	
+		// d2 to base
+		destinyd2.setX(-494.1);
+		destinyd2.setY(100.0);
+		destinyd2.setZ(994.7);
+			
+		return destinyd2;
+		
+	}
+	
+	Point3D target(Point3D dpos){
+		Point3D destiny = new Point3D(0.0,0.0,0.0);
+	
+		destiny.setX(dpos.getX());
+		destiny.setY(dpos.getY());
+		destiny.setZ(dpos.getZ());
+			
+	
 		return destiny;
 		
 	}
 	
-	Point3D getNewPosition(String agent,Point3D d1pos, Point3D d2pos){
-		
-		// Method to calculate the new position. The target of the drone will be the rival's position
-		Point3D destiny = new Point3D(0.0,0.0,0.0);
 
-		if (agent.equals("drone1"))
-		{
-			destiny.setX(d2pos.getX());
-			destiny.setY(d2pos.getY());
-			destiny.setZ(d2pos.getZ());
-			
-		}
-		if (agent.equals("drone2"))
-		{
-			destiny.setX(d1pos.getX());
-			destiny.setY(d1pos.getY());
-			destiny.setZ(d1pos.getZ());
-		}
-		return destiny;
-	}
-
+	
+	
+	
+}
     
 
     
@@ -67,4 +67,3 @@ public class MapModel {
    
 
     
-}
